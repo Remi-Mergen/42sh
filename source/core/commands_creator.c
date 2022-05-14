868af5985f621f534dc0100f67e442627f0d93df
Remi MERGEN <remi.mergen@epitech.eu>
2022-05-14 18:32:47 +0200

    feat: basic command_creator

--- source/core/commands_creator.c
+++ source/core/commands_creator.c
@@ -11,21 +11,25 @@
 
 char **my_stwa(char const *str, char div);
 
-void add_command_in_list(list_commands_t **commands_list, list_commands_t *to_add)
+void add_command_in_list(list_commands_t **commands_list, list_commands_t *command)
 {
+    list_commands_t *commands_node = malloc(sizeof(list_commands_t));
+    commands_node->command = command;
+    commands_node->next = *commands_list;
+    *commands_list = commands_node;
     return;
 }
 
 list_commands_t *commands_creator(mysh_t *mysh, char **env)
 {
-    list_commands_t *commands_list = malloc(sizeof(list_commands_t));
+    list_commands_t *commands_list = NULL;
     command_t *command = malloc(sizeof(command_t));
     char **list = my_stwa(mysh->input, ';');
 
     if (!commands_list)
         return NULL;
     command->env = env;
-    //TODO is_a_builtin_command(command, mysh);
+    //TODO? is_a_builtin_command(command, mysh);
     for (int i = 0; list[i]; ++i) {
         define_path(list[i], &command, mysh);
         // *pipe_handler(list[i], &command, mysh);
