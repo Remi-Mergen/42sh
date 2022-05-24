bb27d5ed5e353c901b2b9e1732e30e93e0fdc6aa
Remi MERGEN <remi.mergen@epitech.eu>
2022-05-24 16:22:23 +0200

    feat: my_exit

--- source/core/commands_creator.c
+++ source/core/commands_creator.c
@@ -11,8 +11,8 @@
 #include "stdlib.h"
 #include "prototype.h"
 #include <stdio.h>//TODO remove
-
-char **my_stwa(char const *str, char div);
+static void get_command(mysh_t **mysh, command_t **command,
+                                            char *input, UNUSED char **env);
 
 static void fill_args(char *input, command_t *command, UNUSED mysh_t *mysh)
 {
@@ -31,7 +31,7 @@ void add_command_in_list(list_commands_t **commands_list, command_t *command)
 static void builtin_command_handler(UNUSED mysh_t *mysh,
                                         command_t *command, char **builtin)
 {
-    for (int i = 0; i < 4; i++) {
+    for (unsigned int i = 0; i < my_array_len(builtin); i++) {
         if (my_strcmp(builtin[i], command->args[0]) == 0) {
             command->builtin = &mysh->builtin_commands[i];
             return;
@@ -41,9 +41,43 @@ static void builtin_command_handler(UNUSED mysh_t *mysh,
     return;
 }
 
+/* static void pipe_handler(char *input, UNUSED command_t **command,
+                        UNUSED mysh_t *mysh, char **env)
+{
+    char **splited = my_stwa(input, '|');
+    if (my_array_len(splited) == 1)
+        return;
+    command_t *pipe_command = malloc(sizeof(command_t));
+    command_t *tmp = NULL;
+    pipe_command->prev_pipe = *command;
+    for (unsigned int i = 1; splited[i]; ++i) {
+        tmp->env = env;
+        get_command(&mysh, &tmp, splited[i], env);
+        pipe_command->next_pipe = tmp;
+        pipe_command = tmp;
+        tmp = malloc(sizeof(command_t));
+        tmp->prev_pipe = pipe_command;
+    }
+    // ?free(tmp);
+} */
+
+static void get_command(mysh_t **mysh, command_t **command,
+                                            char *input, UNUSED char **env)
+{
+    char *builtin[6] = {"cd", "env", "setenv", "unsetenv", "exit", NULL};
+
+    fill_args(input, *command, (*mysh));
+    builtin_command_handler((*mysh), *command, builtin);
+    define_path(input, *command, (*mysh));
+    // pipe_handler(input, command, (*mysh), env);
+    //* redirect_left_handler(input, command, mysh);
+    //! dredirect_left_handler(input, command, mysh);
+    //* redirect_right_handler(input, command, mysh);
+    //* dredirect_right_handler(input, command, mysh);
+}
+
 void commands_creator(mysh_t **mysh, char **env)
 {
-    char *builtin[4] = {"cd", "env", "setenv", "unsetenv"};
     list_commands_t *commands_list = NULL;
     command_t *command = NULL;
     char **list = my_stwa((*mysh)->input, ';');
@@ -51,14 +85,7 @@ void commands_creator(mysh_t **mysh, char **env)
     for (int i = 0; list[i]; ++i) {
         command = malloc(sizeof(command_t));
         command->env = env;
-        fill_args(list[i], command, (*mysh));
-        builtin_command_handler((*mysh), command, builtin);
-        define_path(list[i], command, (*mysh));
-        //* pipe_handler(list[i], &command, mysh);
-        //* redirect_left_handler(list[i], &command, mysh);
-        //* dredirect_left_handler(list[i], &command, mysh);
-        //* redirect_right_handler(list[i], &command, mysh);
-        //* dredirect_right_handler(list[i], &command, mysh);
+        get_command(mysh, &command, list[i], env);
         add_command_in_list(&commands_list, command);
     }
     (*mysh)->commands_list = commands_list;
