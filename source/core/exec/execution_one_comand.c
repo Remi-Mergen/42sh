8463ab829fa61ebf8b7f204b693f07962f9e9808
Remi MERGEN <remi.mergen@epitech.eu>
2022-05-26 20:08:54 +0200

    fix: write in stderr command not found

--- source/core/exec/execution_one_comand.c
+++ source/core/exec/execution_one_comand.c
@@ -7,8 +7,10 @@
 
 #include "lib.h"
 #include "minif.h"
+#include "define.h"
 #include "struct.h"
 #include <unistd.h>
+void close_dup(int *tube, int fd);
 
 void execution_command(mysh_t *mysh, command_t *command)
 {
@@ -17,7 +19,8 @@ void execution_command(mysh_t *mysh, command_t *command)
         command->builtin->fct_ptr(mysh, command);
         return;
     } else if (command->return_value == -1) {
-        minif("%s: Command not found.\n", command->path);
+        write(2, command->path, my_strlen(command->path));
+        write(2, ": Command not found.\n", 21);
         mysh->last_return_value = 1;
         return;
     }
